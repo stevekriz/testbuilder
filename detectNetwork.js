9905e3a8b83edcbf79ea18c62e3c3104db02ddd7
stevekriz <steve.kriz@gmail.com>
2020-10-16 15:28:29 -0700

    Refactored.

--- detectNetwork.js
+++ detectNetwork.js
@@ -22,11 +22,11 @@ var detectNetwork = function(cardNumber) {
     return 'Diner\'s Club';
   } else if (cardNumber.length === 15 && (cardNumber.startsWith('34') || cardNumber.startsWith('37'))) {
     return 'American Express';
-  } else if (cardNumber.length === 16 && (cardNumber.startsWith('51') || cardNumber.startsWith('52') || cardNumber.startsWith('53') || cardNumber.startsWith('54') || cardNumber.startsWith('55'))) {
+  } else if (cardNumber.length === 16 && (cardNumber.slice(0, 2) >= 51 && cardNumber.slice(0, 2) <= 55)) {
     return 'MasterCard';
   } else if ((cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19) && cardNumber.startsWith('4')) {
     return 'Visa';
-  } else if ((cardNumber.length === 16 || cardNumber.length === 19) && (cardNumber.startsWith('6011') || cardNumber.startsWith('644') || cardNumber.startsWith('645') || cardNumber.startsWith('646') || cardNumber.startsWith('647') || cardNumber.startsWith('648') || cardNumber.startsWith('649') || cardNumber.startsWith('65'))) {
+  } else if ((cardNumber.length === 16 || cardNumber.length === 19) && (cardNumber.startsWith('6011') || (cardNumber.slice(0, 3) >= 644 && cardNumber.slice(0, 3) <= 649) || cardNumber.startsWith('65'))) {
     return 'Discover';
   } else if ((cardNumber.length >= 12 && cardNumber.length <= 19) && (cardNumber.startsWith('5018') || cardNumber.startsWith('5020') || cardNumber.startsWith('5038') || cardNumber.startsWith('6304'))) {
     return 'Maestro';
